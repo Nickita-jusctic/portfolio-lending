a5b730f01c2dee6fc5d53d602387c2f80571f3bf
Nickita-jusctic <134242063+Nickita-jusctic@users.noreply.github.com>
2023-05-24 20:33:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+const btnDarkMode = document.querySelector('.dark-mode-btn')
+
+
+//проверка темной темы в localstorage
+if(localStorage.getItem('darkMode') == 'dark'){
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark')
+}
+
+//проверка localstorage на уровне системных настроек
+if(window.matchMedia &&  window.matchMedia("(prefers-color-scheme:dark)").matches){
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark')
+}
+
+
+//включение ночного режима по кнопке
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle('dark')
+
+    if(isDark){
+        localStorage.setItem('darkMode', 'dark')
+    }else{
+        localStorage.setItem('darkMode', 'light')
+    }
+}
